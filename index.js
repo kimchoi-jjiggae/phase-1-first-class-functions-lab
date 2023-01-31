81293ada7e47551ef0a99d5cf63f7ff14902dc27
Michelle <choi.michelle.12@gmail.com>
2023-01-31 13:43:45 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(data){
+    return data.slice(0,2);
+}
+
+function returnLastTwoDrivers(data){
+    return data.slice(data.length-2, data.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return fare => fare*x;
+}
+
+function fareDoubler(fare){
+    const fxn =  createFareMultiplier(2);
+    return fxn(fare);
+}
+
+function fareTripler(fare){
+    const fxn =  createFareMultiplier(3);
+    return fxn(fare);
+}
+
+function selectDifferentDrivers(drivers, fxn){
+    return fxn(drivers);
+}
